088aba317adf08e5e40cd12a1e9f4d17cfe30bfe
Gabriele Greco <gabriele.greco@wyscout.com>
2021-02-10 18:07:02 +0100

    Make a shorter loop

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const native = require("./build/Release/testNapi.node");
 
 console.log("Starting...")
-for (let i = 1; i < 100; ++i) {
+for (let i = 1; i < 12; ++i) {
     const res = native.get();
     console.log("Got ", res.byteLength);
-    if ((i % 10) === 0) {
+    if ((i % 3) === 0) {
         console.log("Calling GC...")
         global.gc();
         console.log("GC completed");
